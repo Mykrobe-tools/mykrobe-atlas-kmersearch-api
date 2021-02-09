935be87360d47a1dee3edafdf8e5407b7564d4e3
Giang Nguyen <giang@ebi.ac.uk>
2021-02-09 19:01:02 +0000

    Add missing async declaration

--- src/api/build.py
+++ src/api/build.py
@@ -11,7 +11,7 @@ class Body(BaseModel):
     cobs_false_positive_rate: float
 
 
-def request(body: Body):
+async def request(body: Body):
     cobs.build(body.cobs_document_dir_path, body.cobs_output_path, body.cobs_term_size, body.cobs_clobber,
                body.cobs_false_positive_rate)
     return body
