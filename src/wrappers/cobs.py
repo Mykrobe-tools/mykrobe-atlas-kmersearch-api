0cea431291bfb4a5f23f1a14e1d607d341a224ea
Giang Nguyen <giang@ebi.ac.uk>
2021-03-23 19:09:07 +0000

    tmp

--- src/wrappers/cobs.py
+++ src/wrappers/cobs.py
@@ -1,10 +1,15 @@
+import subprocess
+import tempfile
+from datetime import datetime
+from distutils.dir_util import copy_tree
 from os import environ
+from pathlib import Path
 from tempfile import TemporaryDirectory
 
 import cobs_index as cobs
 
-
-COBS_INDEX_PATH = environ.get('COBS_INDEX_PATH', '/data/500.cobs_compact')
+COBS_CLASSIC_INDEXES_DIR = environ.get('COBS_CLASSIC_INDEXES_DIR', '/data/classic')
+COBS_INDEX_PATH = environ.get('COBS_INDEX_PATH', '/data/combined.cobs_compact')
 COBS_TERM_SIZE = 31
 COBS_POSITIVE_RATE = 0.4
 
@@ -31,5 +36,17 @@ class Cobs:
             p = cobs.CompactIndexParameters()
             p.term_size = COBS_TERM_SIZE
             p.false_positive_rate = COBS_POSITIVE_RATE
-            p.clobber = True
-            cobs.compact_construct_list(doclist, COBS_INDEX_PATH, p)
+            setattr(p, 'continue', True)
+            classic_path = Path(COBS_CLASSIC_INDEXES_DIR) / datetime.now().strftime("%Y-%m-%d_%H:%M:%S.cobs_classic")
+            cobs.classic_construct_list(doclist, str(classic_path), p)
+
+            # COBS' `compact-construct-combine` command doesn't accept the `keep_temporary` param, even though the real
+            # function does, so we have to resort to making a temporary copy of the classic dir here
+            try:
+                with tempfile.TemporaryDirectory() as tmp_dir:
+                    copy_tree(COBS_CLASSIC_INDEXES_DIR, tmp_dir)
+                    subprocess.check_call(['cobs', 'compact-construct-combine', tmp_dir, COBS_INDEX_PATH])
+            except FileNotFoundError as e:
+                if tmp_dir not in str(e):
+                    raise
+                pass
