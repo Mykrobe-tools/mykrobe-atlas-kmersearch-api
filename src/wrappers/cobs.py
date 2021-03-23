b808192b83de52bba7848d93312abb69a4786636
Giang Nguyen <giang@ebi.ac.uk>
2021-03-23 19:35:57 +0000

    Use multiple classic indexes instead of 1 compact one

--- src/wrappers/cobs.py
+++ src/wrappers/cobs.py
@@ -1,7 +1,4 @@
-import subprocess
-import tempfile
 from datetime import datetime
-from distutils.dir_util import copy_tree
 from os import environ
 from pathlib import Path
 from tempfile import TemporaryDirectory
@@ -9,17 +6,22 @@ from tempfile import TemporaryDirectory
 import cobs_index as cobs
 
 COBS_CLASSIC_INDEXES_DIR = environ.get('COBS_CLASSIC_INDEXES_DIR', '/data/classic')
-COBS_INDEX_PATH = environ.get('COBS_INDEX_PATH', '/data/combined.cobs_compact')
 COBS_TERM_SIZE = 31
 COBS_POSITIVE_RATE = 0.4
 
 
 class Cobs:
-    def __init__(self, index_path=COBS_INDEX_PATH):
-        self.search_instance = cobs.Search(index_path)
+    def __init__(self):
+        index_paths = [p for p in Path(COBS_CLASSIC_INDEXES_DIR).iterdir() if Path.is_file(p)]
+        self.search_instances = [cobs.Search(str(index_path)) for index_path in index_paths]
 
     def search(self, query, threshold):
-        return self.search_instance.search(query, threshold=threshold)
+        results = []
+
+        for search_instance in self.search_instances:
+            results += search_instance.search(query, threshold=threshold)
+
+        return results
 
     @staticmethod
     def build(path_to_file_list, sample_names):
@@ -33,20 +35,9 @@ class Cobs:
             for doc, name in zip(doclist, sample_names):
                 doc.name = name
 
-            p = cobs.CompactIndexParameters()
+            p = cobs.ClassicIndexParameters()
             p.term_size = COBS_TERM_SIZE
             p.false_positive_rate = COBS_POSITIVE_RATE
             setattr(p, 'continue', True)
             classic_path = Path(COBS_CLASSIC_INDEXES_DIR) / datetime.now().strftime("%Y-%m-%d_%H:%M:%S.cobs_classic")
             cobs.classic_construct_list(doclist, str(classic_path), p)
-
-            # COBS' `compact-construct-combine` command doesn't accept the `keep_temporary` param, even though the real
-            # function does, so we have to resort to making a temporary copy of the classic dir here
-            try:
-                with tempfile.TemporaryDirectory() as tmp_dir:
-                    copy_tree(COBS_CLASSIC_INDEXES_DIR, tmp_dir)
-                    subprocess.check_call(['cobs', 'compact-construct-combine', tmp_dir, COBS_INDEX_PATH])
-            except FileNotFoundError as e:
-                if tmp_dir not in str(e):
-                    raise
-                pass
