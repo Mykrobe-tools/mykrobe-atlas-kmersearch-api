441f5b131a64aa760f8e19155fe816f4f4392c2a
Giang Nguyen <giang@ebi.ac.uk>
2021-03-29 18:11:25 +0100

    Manage different-signature size indexes

--- src/wrappers/cobs.py
+++ src/wrappers/cobs.py
@@ -12,7 +12,10 @@ COBS_POSITIVE_RATE = 0.4
 
 class Cobs:
     def __init__(self):
-        index_paths = [p for p in Path(COBS_CLASSIC_INDEXES_DIR).iterdir() if Path.is_file(p)]
+        index_paths = []
+        for path in Path(COBS_CLASSIC_INDEXES_DIR).iterdir():
+            if path.is_dir():
+                index_paths += [p for p in path.iterdir() if p.is_file()]
         self.search_instances = [cobs.Search(str(index_path)) for index_path in index_paths]
 
     def search(self, query, threshold):
@@ -21,10 +24,15 @@ class Cobs:
         for search_instance in self.search_instances:
             results += search_instance.search(query, threshold=threshold)
 
-        return results
+        flatten = []
+        for r in results:
+            flatten.append((r.score, r.doc_name))
+        return flatten
 
     @staticmethod
     def build(sample_paths, sample_names):
+        assert len(sample_paths) == len(sample_names)
+
         with TemporaryDirectory() as tmpdir:
             doclist = cobs.DocumentList(tmpdir)
 
@@ -38,5 +46,10 @@ class Cobs:
             p.term_size = COBS_TERM_SIZE
             p.false_positive_rate = COBS_POSITIVE_RATE
             setattr(p, 'continue', True)
-            classic_path = Path(COBS_CLASSIC_INDEXES_DIR) / datetime.now().strftime("%Y-%m-%d_%H:%M:%S.cobs_classic")
+
+            signature_size = cobs.calc_signature_size(len(sample_paths), p.num_hashes, p.false_positive_rate)
+            classic_path = Path(COBS_CLASSIC_INDEXES_DIR) / str(signature_size)
+            classic_path.mkdir(parents=True, exist_ok=True)
+
+            classic_path /= datetime.now().strftime("%Y-%m-%d_%H:%M:%S.cobs_classic")
             cobs.classic_construct_list(doclist, str(classic_path), p)
